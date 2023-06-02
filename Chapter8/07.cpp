0cf5617d4901eb6bf6356ab928745ad7f52a8d85
goomseo <goomseo1743@naver.com>
2023-06-02 16:22:38 +0900

    Update: Chapter 8

--- Chapter8/07.cpp
+++ Chapter8/07.cpp
@@ -1,3 +1,4 @@
+// 제공되는 답안을 다듬었음.
 #include <iostream>
 using namespace std;
 
@@ -5,31 +6,32 @@ class BaseMemory {
     char *mem;
     int size;
 protected:
-    BaseMemory(int size) {
-        mem = new char [size];
-        this->size = size;
-    }
-    void load(char *mem, int address, int size) {
-        if(address+size > this->size)
-            return; // out of memory range
-        for(int i=0; i<size; i++)
-            this->mem[address + i] = mem[i];
-    }
+    BaseMemory(int size);
+
+    void burn(char *mem, int address, int size);
 public:
-    char read(int address) {
-        return mem[address];
-    } // address 의 범위 체크하지 않았음
+    char read(int address) {return mem[address];}
 };
+
+BaseMemory::BaseMemory(int size)  {
+    mem = new char [size];
+    this -> size = size;
+}
+
+void BaseMemory::burn(char *mem, int address, int size)  {
+    if (address + size > this -> size)
+        return;
+    for (int i = 0; i < size; ++i)
+        this -> mem[address + i] = mem[i];
+}
 
 class ROM : public BaseMemory {
 public:
     ROM(int size, char* initData, int initSize);
 };
 
-ROM::ROM(int size, char* initData, int initSize)
-        : BaseMemory(size)
-{
-    load(initData, 0, initSize); // 0 번지부터 intiSize 데이타 굽기
+ROM::ROM(int size, char* initData, int initSize) : BaseMemory(size) {
+    burn(initData, 0, initSize);
 }
 
 class RAM : public BaseMemory {
@@ -41,14 +43,17 @@ public:
 RAM::RAM(int size) : BaseMemory(size) { }
 
 void RAM::write(int address, char data) {
-    load(&data, address, 1);
+    burn(&data, address, 1);
 }
 
 int main() {
     char x[5]={'h', 'e', 'l', 'l', 'o'};
-    ROM biosROM(1024*10, x, 5); // 10KB의 ROM 메모리. 배열 x로 초기화됨
-    RAM mainMemory(1024*1024); // 1MB의 RAM 메모리
-    // 0 번지에서 4번지까지 biosROM에서 읽어 mainMemory에 복사
-    for(int i=0; i<5; i++) mainMemory.write(i, biosROM.read(i));
-    for(int i=0; i<5; i++) cout << mainMemory.read(i);
+    ROM biosROM(1024 * 10, x, 5);
+    RAM mainMemory(1024 * 1024);
+
+    for(int i = 0; i < 5; ++i)
+        mainMemory.write(i, biosROM.read(i));
+
+    for(int i = 0; i < 5; ++i)
+        cout << mainMemory.read(i);
 }
