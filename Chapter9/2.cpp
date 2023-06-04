a75a7a00deaf383a8dc99f8756eb7728da3928a0
goomseo <goomseo1743@naver.com>
2023-06-04 15:13:49 +0900

    Update: Chapter9

---
+++ Chapter9/2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class Converter {
+protected:
+    double ratio;
+    virtual double convert(double src) = 0;
+    virtual string getSourceString() = 0;
+    virtual string getDestString() = 0;
+public:
+    Converter(double ratio) {this -> ratio = ratio;}
+
+    void run();
+};
+
+void Converter::run() {
+    double src;
+    cout << getSourceString() << "을 " << getDestString() << "로 바꿉니다. ";
+    cout << getSourceString() << "을 입력하세요>> ";
+    cin >> src;
+    cout << "반환 결과: " << convert(src) << getDestString() << endl;
+}
+
+class KmToMile : public Converter {
+private:
+    string source, dest;
+public:
+    KmToMile(double ratio);
+    double convert(double src) override {return src / ratio;}
+    string getSourceString() override {return source;}
+    string getDestString() override {return dest;}
+};
+
+KmToMile::KmToMile(double ratio) : Converter(ratio) {
+    source = "Km";
+    dest = "Mile";
+}
+
+int main() {
+    KmToMile toMile(1.609344);
+    toMile.run();
+}
